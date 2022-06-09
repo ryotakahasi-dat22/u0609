306b5e8d010cc8f638ac5bb084103d919bd17626
reiji-dat <reiji.dat@gmail.com>
2022-06-09 16:09:54 +0900

    ensyu7-3

--- Assets/Hensu.cs
+++ Assets/Hensu.cs
@@ -5,6 +5,31 @@ using UnityEngine;
 public class Hensu : MonoBehaviour
 {
     int a = 10;
+    int vx = 20;
+
+    //順次処理
+    public void Ensyu71()
+    {
+        int vx = 5;
+        Debug.Log(vx);
+
+        vx = 10;
+        Debug.Log(vx);
+
+    }
+
+    public void Ensyu72()
+    {
+
+        Debug.Log(vx);
+    }
+
+    public void Ensyu74()
+    {
+        vx += 10;
+        Debug.Log(vx);
+
+    }
     // Start is called before the first frame update
     public void Proc()
     {
