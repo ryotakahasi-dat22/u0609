d18dd83fa8347b90efb119af77987c0b6420238e
reiji-dat <reiji.dat@gmail.com>
2022-06-09 15:02:14 +0900

    Initial commit

---
+++ Assets/Hensu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hensu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a;
+        float fw;
+        string str;
+        bool bl;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
